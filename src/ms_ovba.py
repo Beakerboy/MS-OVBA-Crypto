edb60a7a3a66afa82d7de6429f4136799669d2f1
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-10 08:18:54 -0500

    Update ms_ovba.py

--- src/ms_ovba.py
+++ src/ms_ovba.py
@@ -12,3 +12,17 @@ def encrypt():
 
 def decrypt():
     pass
+
+
+def encode_nulls(data):
+    """
+    Replace null bytes in data with 0x01.
+    The grbit variable indicates which bytes were replaced.
+    """
+    return grbit, data_no_nulls
+
+def decode_nulls(grbit, data_no_nulls):
+    """
+    Restore null values in the data using grbit.
+    """
+    return data
