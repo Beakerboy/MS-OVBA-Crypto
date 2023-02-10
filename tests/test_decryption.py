2aa64b5196af8ce3b1007050a28347fc1163a456
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-10 14:56:24 -0500

    Update test_decryption.py

--- tests/test_decryption.py
+++ tests/test_decryption.py
@@ -2,5 +2,5 @@ from ms_ovba_crypto import MsOvbaCrypto
 def test_project_visibility():
     enc_data = b'\x15\x17\xCA\xF1\xD6\xF9\xD7\xF9\xD7\x06'
     ms_ovba_crypto = MsOvbaCrypto()
-data = ms_ovba_crypto.decrypt(enc_data)
+    data = ms_ovba_crypto.decrypt(enc_data)
     assert data == b'\xFF'
