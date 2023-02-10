44c67939d28de9c1a70bb3f1fbb3e04025660481
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-10 14:43:04 -0500

    Update test_decryption.py

--- tests/test_decryption.py
+++ tests/test_decryption.py
@@ -1,4 +1,4 @@
-def test_project_visibility()
+def test_project_visibility():
     enc_data = b'\x15\x17\xCA\xF1\xD6\xF9\xD7\xF9\xD7\x06'
     data = ms_ovba_crypt.decrypt(enc_data)
     assert data = b'\xFF'
