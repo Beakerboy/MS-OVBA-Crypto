a4fa6ef13e5189451c8c692f5622009c61b9974b
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-02-11 10:39:14 -0500

    Update test_encryption.py

--- tests/test_encryption.py
+++ tests/test_encryption.py
@@ -1,5 +1,12 @@
 from ms_ovba_crypto import MsOvbaCrypto
 
+
+encryption_data = [
+    ([0x41], b'0xFF', b'\x41\x43\x5A\x5A\x5E\x5A\x5E\x5A\x5E\x5A\x5E'),
+    ([0xBC, 0x7B, 0x7B], b'0xFF', b'\xBC\xBE\xA7\xA2\x59\x1C\x5A\x1C\x5A\x1C'),
+]
+
+
 @pytest.mark.parametrize("rand, data, expected", encryption_data)
 def test_encryption(rand, data, expected):
 
@@ -19,14 +26,3 @@ def test_encryption(rand, data, expected):
     # project.setProtectionState("41435A5A5E5A5E5A5E5A5E")
     # project.setPassword("BCBEA7A2591C5A1C5A1C")
     # project.setVisibilityState("37352C2BDCDD56DE56DEA9")
-
-
-encryption_data = [
-    ([0x41], b'0xFF', b'\x41\x43\x5A\x5A\x5E\x5A\x5E\x5A\x5E\x5A\x5E'),
-    ([0xBC, 0x7B, 0x7B], b'0xFF', b'\xBC\xBE\xA7\xA2\x59\x1C\x5A\x1C\x5A\x1C'),
-]
-
-
-@pytest.mark.parametrize("input, expected", ceilLog2Data)
-def test_cielLog2(input, expected):
-    assert MsOvba.ceilLog2(input) == expected
